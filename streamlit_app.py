b1d6ada86e5a5e46fa6958c19c1fcee69bd95ee4
bhawnatheexplorer <129192336+bhawnatheexplorer@users.noreply.github.com>
2023-03-28 17:16:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents are healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(🥣'Breakfast Menu')
+streamlit.text(🥗'Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞'Avacado Toast')
